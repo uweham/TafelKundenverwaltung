92242fb64c263844102c006c8ffa9c2259bd517d
uweham <uwe.hobby@gmx.de>
2026-03-02 18:26:28 +0100

    new class for get app.nointernet etc from version.properties

---
+++ src/main/java/kundenverwaltung/service/GetVersionProperties.java
@@ -0,0 +1,39 @@
+package kundenverwaltung.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GetVersionProperties {
+ 
+    {
+      try (InputStream input = getClass().getResourceAsStream("/version.properties")) {
+    
+        Properties props = new Properties();
+        if (input != null) {
+            props.load(input);
+            String nointernet = props.getProperty("app.nointernet", null);
+            if (nointernet != null) {
+              System.setProperty("app.nointernet", nointernet);
+            }
+        }
+      } catch (IOException ex) {
+          System.err.println("Version konnte nicht geladen werden.");
+      }
+    }
+  
+    /**
+    * checks in version.properties the key app.nointernet 
+    * Returns true when app.nointernet set otherwise false 
+    *
+    * @return      true when app.nointerent set otherwise false 
+    */   
+    
+  public boolean isNoInternetEnvironment() {
+    String v = System.getProperty("app.nointernet");
+    System.out.println(v);
+    return v != null && v.startsWith("${"); 
+    
+    
+  }
+}
