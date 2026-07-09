9c79873bb64eb7296452d58c55ceded773d16cdf
uweham <uwe.hobby@gmx.de>
2026-07-09 18:28:23 +0200

    add constants

--- src/main/java/kundenverwaltung/service/Constants.java
+++ src/main/java/kundenverwaltung/service/Constants.java
@@ -50,5 +50,6 @@ public class Constants {
   public static final int STATISTIK_AMOUNTS_CREDITS=1;
   public static final int STATISTIK_AMOUNTS_OUTSTANDING = 2;
   
+  public static final int WARENTYPID_GUTSCHRIFT = 3; // change this value when change in sql table warentyp
   
 }
